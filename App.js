2dac6fd677f9f6eca7bb040572a7d20a853d4374
abdullahshayk <abdullahazhar971@gmail.com>
2020-11-28 20:44:47 +0500

    task1

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
+export default class App extends Component {
+  render() {
   return (
     <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize:22, fontWeight:"bold" }}>DISCOUNT CALCULATOR APP</Text>
+      <View style={{flexDirection:'row', }}>
+      <TextInput style={styles.input} placeholder="Original price" placeholderTextColor="blue"/>
+      <TextInput style={styles.input} placeholder="Discount %" placeholderTextColor="blue"/>
+        </View>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +23,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth:1,
+    margin:7,
+    borderRadius:9,
+    height:30,
+    width:130,
+    textAlign:"center"
+    
+  }
 });
