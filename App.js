d28e7c992bc510aad139b3032545e44cb3f9c468
abdullahshayk <abdullahazhar971@gmail.com>
2020-11-28 22:07:33 +0500

    task2

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default class App extends Component {
+  constructor(prop){
+    super(prop);
+    this.state={
+      OriginalPrice:0,
+      DiscountPercentage:0,
+      YouSave:"You Save",
+      FinalPrice:"Final Price"
+    }
+  }
+
+  calculate=()=>{
+    const discount=this.state.OriginalPrice-(this.state.OriginalPrice*(this.state.DiscountPercentage/100));
+    const saved=(this.state.OriginalPrice-this.state.OriginalPrice-(this.state.OriginalPrice*(this.state.DiscountPercentage/100)))
+    this.setState({
+      FinalPrice:discount,
+      YouSave:Math.abs(saved)
+    })
+
+  }
   render() {
   return (
     <View style={styles.container}>
       <Text style={{fontSize:22, fontWeight:"bold" }}>DISCOUNT CALCULATOR APP</Text>
+  
       <View style={{flexDirection:'row', }}>
-      <TextInput style={styles.input} placeholder="Original price" placeholderTextColor="blue"/>
-      <TextInput style={styles.input} placeholder="Discount %" placeholderTextColor="blue"/>
+        
+      <TextInput style={styles.input} placeholder="Original price" placeholderTextColor="blue" keyboardType="number-pad"
+      onChangeText={(OriginalPrice)=> this.setState({OriginalPrice})}/>
+
+      <TextInput style={styles.input} placeholder="Discount %" placeholderTextColor="blue" keyboardType="number-pad"
+       onChangeText={(DiscountPercentage)=> this.setState({DiscountPercentage})}/>
+
         </View>
+<TouchableOpacity style={styles.CalculateBTN} onPress={()=>this.calculate()}><Text style={{fontSize:20}}>Calculate</Text></TouchableOpacity>
+
+        <View >
+  <Text style={styles.output}>{this.state.FinalPrice}</Text>
+  <Text style={styles.output}>{this.state.YouSave}</Text>
+
+  </View>
     </View>
   );
 }
@@ -29,7 +61,30 @@ const styles = StyleSheet.create({
     borderRadius:9,
     height:30,
     width:130,
-    textAlign:"center"
+    textAlign:"center",
+   
     
+  },
+  CalculateBTN:{
+    backgroundColor:"#87cefa",
+    height:30,
+    width:120,
+    textAlign:"center",
+    alignItems:"center",
+    borderRadius:12,
+    margin:10
+
+  },
+  output:{
+    
+    fontSize:22,
+    marginTop:20,
+    borderWidth:1,
+    padding:10,
+    borderColor:"black",
+    justifyContent:"center",
+    alignItems:"center",
+    textAlign:"center"
+
   }
 });
