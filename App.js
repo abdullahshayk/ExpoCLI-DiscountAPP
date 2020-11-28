de796c98f391d4c986234143627689b8ddd69b52
abdullahshayk <abdullahazhar971@gmail.com>
2020-11-28 22:35:04 +0500

    task3 and task4

--- App.js
+++ App.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 
 export default class App extends Component {
   constructor(prop){
     super(prop);
     this.state={
       OriginalPrice:0,
-      DiscountPercentage:0,
+      DiscountPercentage:"",
       YouSave:"You Save",
       FinalPrice:"Final Price"
     }
   }
 
   calculate=()=>{
-    const discount=this.state.OriginalPrice-(this.state.OriginalPrice*(this.state.DiscountPercentage/100));
+    if(this.state.OriginalPrice===0 || this.state.DiscountPercentage==="" ){
+      alert("EMPTY FIELDS!!")
+    }
+    else if(this.state.OriginalPrice<0){
+      alert("price cannot be less than 0")
+
+    }
+    else if(this.state.DiscountPercentage>100){
+      alert("Discount is never greater than 100")
+
+    }
+    else if(this.state.DiscountPercentage<0){
+      alert("Discount is never lesser than 0")
+
+    }
+    else{
+      const discount=this.state.OriginalPrice-(this.state.OriginalPrice*(this.state.DiscountPercentage/100));
     const saved=(this.state.OriginalPrice-this.state.OriginalPrice-(this.state.OriginalPrice*(this.state.DiscountPercentage/100)))
     this.setState({
       FinalPrice:discount,
       YouSave:Math.abs(saved)
     })
 
-  }
+  }}
   render() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:22, fontWeight:"bold" }}>DISCOUNT CALCULATOR APP</Text>
+      <Text style={{fontSize:22, fontWeight:"bold", marginBottom:45 }}>DISCOUNT CALCULATOR APP</Text>
   
       <View style={{flexDirection:'row', }}>
         
@@ -67,19 +83,21 @@ const styles = StyleSheet.create({
   },
   CalculateBTN:{
     backgroundColor:"#87cefa",
-    height:30,
+    padding:8,
     width:120,
     textAlign:"center",
     alignItems:"center",
-    borderRadius:12,
+    borderRadius:19,
     margin:10
 
   },
   output:{
     
     fontSize:22,
-    marginTop:20,
+    marginTop:40,
     borderWidth:1,
+    color:"grey",
+    
     padding:10,
     borderColor:"black",
     justifyContent:"center",
